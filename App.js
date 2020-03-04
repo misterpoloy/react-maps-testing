d5319a7704bd0ff8fb50a152e110f0c38f49db9d
Juan Pablo Ortiz <jp@calaps.com>
2020-03-04 12:20:16 -0600

    basic map layout

--- App.js
+++ App.js
@@ -1,49 +1,23 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from 'react';
+import MapView from 'react-native-maps'
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { View } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const App = () =>
+(
+  <View style={{ flex: 1 }}>
+    <MapView
+      style={{ flex: 1 }}
+      region={{
+          latitude: 37.785834,
+          longitude: -122.406417,
+          latitudeDelta: 0.0143,
+          longitudeDelta: 0.0134,
+      }}
+      showsUserLocation
+      loadingEnabled
+    />
+  </View>
+);
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export default App;
